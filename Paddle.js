bd685d424e1b49571e08a64f2ec5234cfb4d4dc4
EQMoore <117695748+EQMoore@users.noreply.github.com>
2024-07-30 16:15:27 -0400

    a

--- Paddle.js
+++ Paddle.js
@@ -1,16 +1,43 @@
 class Paddle {
-    
+    constructor(x, y, w, h, side, c) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.side = side;
+        this.c = c;
+        this.vy = 0;
+    }
+
+
+draw(ctx) {
+    ctx.fillStyle = this.c;
+    ctx.strokeStle = "black";
+    ctx.lineWidth = 2;
+
+    ctx.fillRect(this.x, this.y, this.w, this.h);
+    ctx.strokeRect(this.x, this.y, this.w, this.h);
+}
 
     move(isCPU, ball) {
         if (isCPU) {
+            if(this.y > ball.y){
+                this.vy = paddleVelocity;
+            }
+            else if(this.y < ball.y){
+                this.vy = -paddleVelocity;
+            }
+            
             // ball.y <- where the ball is
             // this.y <- where the paddle is
             // this.l <- how long the paddle is
 
             // control this.vy using ball
             // don't set this.y! (cheating)
-            this.vy = paddleVelocity; // <-- change this
+             // <-- change this
         }
-        
+        this.y += this.vy;
+        if (this.y <0) this.y = 0;
+        if (this.y + this.h > boardHeight) this.y = boardHeight - this.h;
     }
 }
--- pong.js
+++ pong.js
@@ -1,14 +1,15 @@
 const gameboard = document.getElementById("gameboard");
 const cpucheck = document.getElementById("cpucheck");
 const ctx = gameboard.getContext("2d");
-
+const STATE = {STARTUP: 0, PLAYING: 1, GAMEOVER: 2};
+const SIDE = {NONE: 0, LEFT: 1, RIGHT: 2};
 
 let boardWidth = 500;
 let boardHeight = 500;
 let paddleWidth = 25;
 let paddleLength = 100;
 let ballRadius = 12.5;
-let paddleVelocity = 5;
+let paddleVelocity = 10;
 let paddleSpin = 1.5; // >= 0.0
 let paddleForce = 1.1; // >= 1.0
 
@@ -17,19 +18,77 @@ let paddleL;
 let paddleR;
 let scoreL = 0;
 let scoreR = 0;
+let state = STATE.STARTUP;
 
 function resetGame() {
-    
+    state = STATE.STARTUP;
+    clearInterval(intervalID);
+    nextTick();
 }
 
+let intervalID;
 function nextTick() {
-    
+    switch (state) {
+        case STATE.STARTUP:
+            state = startup();
+            break;
+        case STATE.PLAYING:
+            state = playing();
+            break;
+        case STATE.GAMEOVER:
+            state = gameover();
+            break;
+        default:
+            state = STATE.STARTUP;
+            break;
+
+    }
+    intervalID = setTimeout(nextTick, 10)
 }
 
-function play() {
-    
+function startup() {
+    ball = new Ball(boardWidth/2, boardHeight/2, 1, -1, ballRadius, "hotpink");
+    paddleL = new Paddle(0, 0, paddleWidth, paddleLength, SIDE.LEFT, "red");
+    paddleR = new Paddle(boardWidth-paddleWidth, 0 , paddleWidth, paddleLength, SIDE.RIGHT, "blue");
+    // boardHeight-paddleLength
+    draw();
+    return STATE.PLAYING;
 }
 
+function playing() {
+    paddleL.move(false, ball);
+    paddleR.move(cpucheck.ariaChecked, ball)
+    let sideScore = ball.bounce([paddleL, paddleR]);
+    if(sideScore != SIDE.NONE){
+        if (sideScore == SIDE.LEFT){
+             scoreL++;
+             ball = new Ball(boardWidth/2, boardHeight/2, -1, -1, ballRadius, "hotpink")
+        }
+        if (sideScore == SIDE.RIGHT){
+            scoreR++;
+            ball = new Ball(boardWidth/2, boardHeight/2, 1, -1, ballRadius, "hotpink")
+        } 
+    }
+    updateScore();
+    ball.bounce([paddleL, paddleR]);
+    ball.move();
+    draw();
+    if (scoreL > 2 || scoreR > 2) return STATE.GAMEOVER;
+    return STATE.PLAYING;
+}
+
+function gameover() {
+    return STATE.GAMEOVER;
+}
+
+function draw() {
+    ctx.fillStyle = "grey";
+    ctx.fillRect(0, 0, boardWidth, boardHeight);
+    ball.draw(ctx);
+    paddleL.draw(ctx);
+    paddleR.draw(ctx);
+}
+ 
 function updateScore() {
     const scoreboard = document.getElementById("scoreboard");
     scoreboard.innerHTML = `${scoreL} : ${scoreR}`; // 7 : 3
